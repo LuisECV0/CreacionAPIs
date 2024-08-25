9af2ec77c435fe878a7fe3bad6686179738feac3
Luiscv1719 <luiscv1719@gmail.com>
2024-08-24 23:50:13 -0500

    se añadieron controladores

---
+++ ProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 800 },
+            new Product { Id = 2, Name = "Smartphone", Price = 500 }
+        };
+
+        // GET: api/products
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return Ok(products);
+        }
+
+        // GET: api/products/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        // POST: api/products
+        [HttpPost]
+        public ActionResult<Product> PostProduct(Product product)
+        {
+            product.Id = products.Count + 1;
+            products.Add(product);
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        // PUT: api/products/{id}
+        [HttpPut("{id}")]
+        public ActionResult PutProduct(int id, Product updatedProduct)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            product.Name = updatedProduct.Name;
+            product.Price = updatedProduct.Price;
+            return NoContent();
+        }
+
+        // DELETE: api/products/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            products.Remove(product);
+            return NoContent();
+        }
+    }
+}
--- ProductAPI/obj/Debug/net8.0/ProductAPI.MvcApplicationPartsAssemblyInfo.cs
+++ ProductAPI/obj/Debug/net8.0/ProductAPI.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ProductAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4caa5a7ba7e6fbf8c34e5d070ed18a46be5cf5")]
-[assembly: System.Reflection.AssemblyProductAttribute("ProductAPI")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ProductAPI")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
